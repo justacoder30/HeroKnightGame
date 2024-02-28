104f0cc664f05485d428342da2b849f4b48d40f2
justacoder30 <dangquang0129@gmail.com>
2024-02-28 11:43:10 +0700

    Updated

--- HeroKnightGame/InputManager.cs
+++ HeroKnightGame/InputManager.cs
@@ -1,7 +1,14 @@
-﻿namespace HeroKnightGame
-{
-    public class InputManager
-    {
+﻿using Microsoft.Xna.Framework.Input;
 
+namespace HeroKnightGame
+{
+    public static class InputManager
+    {
+        public static void Update()
+        {
+            var KeySate = Keyboard.GetState();
+
+            if(KeySate.IsKeyDown(Keys.F11)) Renderer.FullScreenToggle();
+        }
     }
 }
--- HeroKnightGame/Renderer.cs
+++ HeroKnightGame/Renderer.cs
@@ -1,6 +1,63 @@
-﻿namespace HeroKnightGame
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace HeroKnightGame
 {
     public class Renderer
     {
+        private static RenderTarget2D _target;
+        private static Rectangle Rectangle;
+        private static bool FullScreen = false;
+
+        public Renderer()
+        {
+            _target = new RenderTarget2D(Globals.Graphics.GraphicsDevice, Globals.WindowSize.X, Globals.WindowSize.Y);
+        }
+
+        public static void SetDestinationRectangle()
+        {
+            var screenSize = Globals.Graphics.GraphicsDevice.PresentationParameters.Bounds;
+
+            float scaleX = (float)screenSize.Width / _target.Width;
+            float scaleY = (float)screenSize.Height / _target.Height;
+            float scale = Math.Min(scaleX, scaleY);
+
+            int newWidth = (int)(_target.Width * scale);
+            int newHeight = (int)(_target.Height * scale);
+
+            //int posX = (screenSize.Width - newWidth) / 2;
+            //int posY = (screenSize.Height - newHeight) / 2;
+
+            Rectangle = new Rectangle(0, 0, newWidth, newHeight);
+        }
+
+        public void SetResolution(int height, int width)
+        {
+            Globals.Graphics.PreferredBackBufferWidth = height;
+            Globals.Graphics.PreferredBackBufferHeight = width;
+            Globals.Graphics.ApplyChanges();
+            SetDestinationRectangle();
+        }
+
+        public static void FullScreenToggle()
+        {
+            FullScreen = !FullScreen;
+            Globals.Graphics.IsFullScreen = FullScreen;
+            Globals.Graphics.ApplyChanges();
+        }
+
+        public void Activate()
+        {
+            Globals.Graphics.GraphicsDevice.SetRenderTarget(_target);
+        }
+
+        public void Draw()
+        {
+            Globals.Graphics.GraphicsDevice.SetRenderTarget(null);
+            Globals.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            Globals.SpriteBatch.Draw(_target, Rectangle, Color.White);
+            Globals.SpriteBatch.End();
+        }
     }
 }
